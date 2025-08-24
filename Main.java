fad3f79ce914e13b7fb682672e19566d315d29c2
MastaDimise <dmitriyrakaev@gmail.com>
2025-08-24 15:40:03 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // задание 1 (обязательное)
+        System.out.println("Dmitriy Rakaev");
+        // задание 2 (не обязательное)
+        int income = 40000, spending = 25000, balance = income - spending;
+        System.out.println("Итого (руб):");
+        System.out.println(balance);
+    }
+}
